ad563aad0f9fd7772868d54c1431a38f8f566c29
Adam Russell <adamlr6+pub@gmail.com>
2015-03-08 20:56:32 +0200

    Remove FizzBuzz special case (divisible by 15).

--- haskell/src/main/Main.hs
+++ haskell/src/main/Main.hs
@@ -1,12 +1,23 @@
+import Control.Applicative ((<*>))
+import Data.Maybe (fromMaybe)
+import Data.Monoid (mappend)
+
 divisibleBy :: Int -> Int -> Bool
 m `divisibleBy` n = m `mod` n == 0
 
+whenDivisibleBy :: String -> Int -> Int -> Maybe String
+whenDivisibleBy text divisor dividend =
+  if dividend `divisibleBy` divisor
+    then Just text
+    else Nothing
+
+fizzBuzzReplacementString :: Int -> Maybe String
+fizzBuzzReplacementString = mappend
+  ("Fizz" `whenDivisibleBy` 3)
+  ("Buzz" `whenDivisibleBy` 5)
+
 fizzBuzzElement :: Int -> String
-fizzBuzzElement n
-  | n `divisibleBy` 15 = "FizzBuzz"
-  | n `divisibleBy` 3 = "Fizz"
-  | n `divisibleBy` 5 = "Buzz"
-  | otherwise = show n
+fizzBuzzElement = fromMaybe . show <*> fizzBuzzReplacementString
 
 fizzBuzzList :: [String]
 fizzBuzzList = map fizzBuzzElement [1..100]
