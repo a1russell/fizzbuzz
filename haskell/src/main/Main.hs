0ccae057b1935f3c97d9a85ec89302a58d32e27f
Adam Russell <adamlr6+pub@gmail.com>
2015-03-08 20:59:01 +0200

    Revert "Remove FizzBuzz special case."
    Removing the special case makes the code too difficult to understand.
    
    This reverts commit ad563aad0f9fd7772868d54c1431a38f8f566c29.

--- haskell/src/main/Main.hs
+++ haskell/src/main/Main.hs
@@ -1,23 +1,12 @@
-import Control.Applicative ((<*>))
-import Data.Maybe (fromMaybe)
-import Data.Monoid (mappend)
-
 divisibleBy :: Int -> Int -> Bool
 m `divisibleBy` n = m `mod` n == 0
 
-whenDivisibleBy :: String -> Int -> Int -> Maybe String
-whenDivisibleBy text divisor dividend =
-  if dividend `divisibleBy` divisor
-    then Just text
-    else Nothing
-
-fizzBuzzReplacementString :: Int -> Maybe String
-fizzBuzzReplacementString = mappend
-  ("Fizz" `whenDivisibleBy` 3)
-  ("Buzz" `whenDivisibleBy` 5)
-
 fizzBuzzElement :: Int -> String
-fizzBuzzElement = fromMaybe . show <*> fizzBuzzReplacementString
+fizzBuzzElement n
+  | n `divisibleBy` 15 = "FizzBuzz"
+  | n `divisibleBy` 3 = "Fizz"
+  | n `divisibleBy` 5 = "Buzz"
+  | otherwise = show n
 
 fizzBuzzList :: [String]
 fizzBuzzList = map fizzBuzzElement [1..100]
